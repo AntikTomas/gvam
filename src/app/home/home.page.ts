c453825be62542b1f3dd6a739ce3bd93e5b65cc0
AntikTomas <tomas.antik@gmail.com>
2024-11-16 17:11:23 +0100

    feat: home page - added components module, added mapmodalmodule into components module, implemented modal with modal controller

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -3,6 +3,8 @@ import { ScreenOrientation } from '@capacitor/screen-orientation';
 
 import Swiper from 'swiper';
 import { FontSizeService } from '../services/font-size.service';
+import { ModalController } from '@ionic/angular';
+import { MapModalComponent } from '../components/map-modal/map-modal.component';
 
 @Component({
   selector: 'app-home',
@@ -21,12 +23,13 @@ export class HomePage {
     'https://historia.nationalgeographic.com.es/medio/2020/01/24/virgina-woolf-padecio-un-trastorno-bipolar-con-fases-depresivas-severas_9da23532_550x751.jpg', 
     'https://upload.wikimedia.org/wikipedia/commons/thumb/d/d3/Albert_Einstein_Head.jpg/576px-Albert_Einstein_Head.jpg',
     'https://upload.wikimedia.org/wikipedia/commons/thumb/c/c8/Marie_Curie_c._1920s.jpg/640px-Marie_Curie_c._1920s.jpg',
-  ]
+  ];
 
   fontSize: number;
 
   constructor(
     private fontService: FontSizeService,
+    private mdlCtrl: ModalController,
   ) {
     this.fontSize = this.fontService.fontSize;
   }
@@ -63,6 +66,15 @@ export class HomePage {
 
   minReached() {
     return this.fontService.minSizeReached();
+  }
+
+  async openModal() {
+    const modal = await this.mdlCtrl.create({
+      component: MapModalComponent,
+      initialBreakpoint: 0.5,
+      breakpoints: [0.5, 0.75, 1]
+    });
+    modal.present();
   }
 
 }
